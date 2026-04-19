c9f1e24bd8b16118153fffa50baa474f0cb75e49
Gideon van den Herik <g.vdherik@live.nl>
2026-04-20 00:54:39 +0200

    Revert: CameraController code

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,53 +3,17 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private List<Camera> _cameras = new List<Camera>();
-    private List<Vector3> _offsets = new List<Vector3>();
     public GameObject player;
     public Vector3 cameraOffset;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        GameObject[] camObjects = GameObject.FindGameObjectsWithTag("Camera");
-
-        foreach (GameObject camObject in camObjects)
-        {
-            Camera cam = camObject.GetComponent<Camera>();
-            
-            if (cam == null) continue;
-            
-            _cameras.Add(cam);
-            
-            Vector3 offset = cam.transform.position - player.transform.position;
-            _offsets.Add(offset);
-            
-            cam.enabled = false;
-        }
-
-        ActivateCamera(0);
-    }
+    { }
 
     // Update is called once per frame
     void LateUpdate()
     { 
         transform.position = player.transform.position + cameraOffset;
         transform.rotation = player.transform.rotation;
-    }
-
-    void ActivateCamera(int index)
-    {
-        Debug.Log($"Activating camera \"{_cameras[index].name}\"");
-        for (int i = 0; i < _cameras.Count; i++)
-        {
-            bool active = (i == index);
-            _cameras[i].enabled = active;
-            
-            var listener = _cameras[i].GetComponent<AudioListener>();
-            
-            if (listener == null) continue;
-            
-            listener.enabled = active;
-        }
     }
 }
